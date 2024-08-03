d787cf4d866e38111f1aa4d7ef7ead46477ff283
Lei Huang <lh389@cornell.edu>
2024-08-03 14:34:39 -0700

    update files

--- mooncake/script.js
+++ mooncake/script.js
@@ -11,3 +11,28 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+const div = document.createElement('div');
+div.id = 'playerContainer';
+
+const audioEl = document.createElement('audio');
+audioEl.controls = true;
+audioEl.id = 'player';
+audioEl.src = 'recording.m4a';
+div.appendChild(audioEl);
+
+const captions = document.createElement('div');
+captions.id = 'captions';
+
+const trackEl = document.createElement('track');
+trackEl.default = true;
+trackEl.kind = 'captions';
+trackEl.src = 'recording.vtt';
+trackEl.addEventListener('cuechange', function (event) {
+  const text = event.target?.track?.activeCues?.[0]?.text;
+  captions.innerText = text ?? '';
+});
+audioEl.appendChild(trackEl);
+
+document.querySelector('body').appendChild(div);
+document.querySelector('body').appendChild(captions);
